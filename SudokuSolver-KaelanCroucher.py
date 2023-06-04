3ca910a9dd6c37bd2f8116c20cdb41e2f4d5ceea
KaelanCroucher <kaelancroucher@protonmail.com>
2023-06-04 16:08:44 -0600

    Made some changes to number of tiles being generated

--- SudokuSolver-KaelanCroucher.py
+++ SudokuSolver-KaelanCroucher.py
@@ -24,7 +24,7 @@ def printBoard(sudokuBoard):
         print(str(sudokuBoard[i][j]), end = " ")
   
   # Print ending line of the puzzle
-  print("X-----------------------X\n")
+  print("X-----------------------X\n\n")
 
 # Method to Validate each tile placed on the board.
 def validateTile(currentBoard, row, column, tileValue ):
@@ -49,8 +49,9 @@ def validateTile(currentBoard, row, column, tileValue ):
 
 
 # This function generates a 9X9 grid with 17 filled tiles randomly, validating each tile with Validate Tile.
-# It fills 34 tiles, originally I went with 17 as this is the minimum number of tiles for a unique puzzle solution.
-# However 17/81 tiles felt sparse.
+# It fills 24 tiles, originally I went with 17 as this is the minimum number of tiles for a unique puzzle solution.
+# However 17/81 tiles felt sparse note: I have played around with this value and 24 seems to be the highest number of
+# tiles that my program can relaiably create a solvable puzzle.
 def newSudokuBoard():
   #This defines the base 9x9 grid where each 0 represents an empty tile.
   emptyBoard = [[0,0,0,0,0,0,0,0,0],
@@ -66,17 +67,20 @@ def newSudokuBoard():
   # Assign empty board value to the initial board to filled.
   initialBoard = emptyBoard
 
-  # Fill the initialBoard with 34 tiles validated to follow sudoku rules.
-  for _ in range(34):
+  filledTiles = 0
+  # Fill the initialBoard with 24 tiles validated to follow sudoku rules.
+  while filledTiles < 24:
     # Generate random column number, row number and tile value using rand library.
     row, column, tileValue = rand.randint(0, 8), rand.randint(0,8), rand.randint(1,9)
 
     # If the previous generation was not valid, generate one until it is valid.
-    while not validateTile(emptyBoard, row, column, tileValue):
+    while not validateTile(initialBoard, row, column, tileValue):
       row, column, tileValue = rand.randint(0, 8), rand.randint(0,8), rand.randint(1,9)
 
     #If valid generation assign value to tile on board.
-    initialBoard[row][column] = tileValue
+    if initialBoard[row][column] == 0:
+      initialBoard[row][column] = tileValue
+      filledTiles += 1
 
   #Print the newly generated initial puzzle board.
   print("Inital Board:")
@@ -89,7 +93,7 @@ def solveSudokuPuzzle(sudokuPuzzle, row = 0, column = 0):
   
   if column == 9:
     if row == 8:
-      print("Solved Board")
+      print("Solved Board:")
       printBoard(sudokuPuzzle)
       return True
     row +=1
@@ -108,7 +112,8 @@ def solveSudokuPuzzle(sudokuPuzzle, row = 0, column = 0):
   return False
 
 print("Welcome to Kaelan's Sudoku Solver.")
-print("The program will generate a random Sudoku Puzzle with 34/81 tiles filled.")
+print("The program will generate a random Sudoku Puzzle with 24/81 tiles filled.")
 print("Then the program will use the backtracking algorithm to solve the puzzle.\n")
 
-solveSudokuPuzzle(newSudokuBoard())
+if solveSudokuPuzzle(newSudokuBoard()) == False:
+  print("Sudoku Board is Unsolvable.\n")
